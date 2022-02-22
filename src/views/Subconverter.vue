36a09d700496d494e12a3c317213007b24cb8a4c
wetryui89 <100178248+wetryui89@users.noreply.github.com>
2022-02-22 05:56:45 +0000

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://subweb-heroku.yunbai.workers.dev/sub?" }],
+        backendOptions: [{ value: "https://royal-voice-78c8.diuekcnoicx67.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
